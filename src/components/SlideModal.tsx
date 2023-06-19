b20cce9efe9376caaa44108a4e0c32ac9c88cf80
sakho13 <wecl.khotaro@gmail.com>
2023-06-19 13:24:28 +0900

    SlideModal修正

--- src/components/SlideModal.tsx
+++ src/components/SlideModal.tsx
@@ -1,19 +1,37 @@
+import { Text, View } from "react-native"
 import Modal from "react-native-modal"
+import { useSettingsValue } from "../contexts/settingContext"
 
 type Props = {
   isVisible: boolean
   children: React.ReactNode
+  head: string
   onClose: () => void
 }
 
-const SlideModal: React.FC<Props> = ({ isVisible, onClose, children }) => {
+const SlideModal: React.FC<Props> = ({
+  isVisible,
+  head,
+  onClose,
+  children,
+}) => {
+  const { backgroundColor, textColor } = useSettingsValue()
+
   return (
     <Modal
       isVisible={isVisible}
       onSwipeComplete={onClose}
       swipeDirection={"down"}
     >
-      {children}
+      <View
+        style={{ backgroundColor, paddingHorizontal: 12, paddingVertical: 16 }}
+      >
+        <Text style={{ color: textColor, fontWeight: "bold", fontSize: 18 }}>
+          {head}
+        </Text>
+
+        {children}
+      </View>
     </Modal>
   )
 }
