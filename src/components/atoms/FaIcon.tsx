ff1a8c1f8b6171c2547cc660404490b4913e93df
sakho13 <wecl.khotaro@gmail.com>
2023-06-25 11:43:15 +0900

    FaIcon 引数追加

--- src/components/atoms/FaIcon.tsx
+++ src/components/atoms/FaIcon.tsx
@@ -1,13 +1,15 @@
+import { StyleProp, TextStyle } from "react-native"
 import Icon from "react-native-vector-icons/FontAwesome5"
 
 type Props = {
   name: string
   size?: number
   color?: string
+  style?: StyleProp<TextStyle>
 }
 
-const FaIcon: React.FC<Props> = ({ name, size, color }) => {
-  return <Icon name={name} size={size} color={color} />
+const FaIcon: React.FC<Props> = ({ name, size, color, style }) => {
+  return <Icon name={name} size={size} color={color} style={style} />
 }
 
 export default FaIcon
