8aa096464739d534453297a0f350b3814dc18902
sakho13 <wecl.khotaro@gmail.com>
2023-06-30 22:43:31 +0900

    ✅ Add 共通関数のテスト

--- tests/functions/commonFunctions.test.ts
+++ tests/functions/commonFunctions.test.ts
@@ -1,24 +1,39 @@
-import { isColorText, zeroOffset } from "../../src/functions/commonFunctions"
+import {
+  isColorText,
+  parseDateFormatText,
+  zeroOffset,
+} from "../../src/functions/commonFunctions"
 
 describe("共通関数", () => {
-  test("zeroOffset(123, 5) = 00123", () => {
+  test("[success] zeroOffset", () => {
     const num = 123
     const str = zeroOffset(num, 5)
     expect(str).toBe("00123")
   })
 
-  test("isColorText(#0fa00f) is color", () => {
+  test("[success] isColorText", () => {
     const isColor = isColorText("#0fa00f")
     expect(isColor).toBe(true)
   })
 
-  test("isColorText(0fa00f) is not color", () => {
+  test("[success] isColorText", () => {
     const isColor = isColorText("0fa00f")
     expect(isColor).toBe(false)
   })
 
-  test("isColorText(#0fg00f) is not color", () => {
+  test("[fail] isColorText", () => {
     const isColor = isColorText("#0fg00f")
     expect(isColor).toBe(false)
+  })
+
+  test("[success] parseDateFormatText", () => {
+    const date = parseDateFormatText("9999/12/31 24:59（改）")
+    expect(date).toBe("9999/12/31 24:59")
+  })
+
+  test("[fail] parseDateFormatText", () => {
+    const date = parseDateFormatText("9999-12/31 24:59（改）")
+    expect(date).not.toBe("9999/12/31 24:59")
+    expect(date).toBe("")
   })
 })
