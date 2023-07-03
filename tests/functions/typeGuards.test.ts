21dae84f5d7d3c1696fccc0d1ca37b62c63e1fe0
sakho13 <wecl.khotaro@gmail.com>
2023-07-04 00:44:30 +0900

    ✅ Add 型ガードテスト

--- tests/functions/typeGuards.test.ts
+++ tests/functions/typeGuards.test.ts
@@ -1,11 +1,54 @@
-import { isNarouAPICountPart } from "../../src/functions/typeGuards"
+import {
+  isNarouAPICountPart,
+  isNovelEpisodeChapter,
+  isNovelEpisodeTitle,
+} from "../../src/functions/typeGuards"
 
 describe("型ガード", () => {
-  test("isNarouAPICountPart({allcount: 0}) is OK", () => {
+  test("[success] isNarouAPICountPart", () => {
     expect(isNarouAPICountPart({ allcount: 0 })).toBe(true)
   })
 
-  test("isNarouAPICountPart({allcounts: 0}) is wrong", () => {
+  test("[fail] isNarouAPICountPart", () => {
     expect(isNarouAPICountPart({ allcounts: 0 })).toBe(false)
+  })
+
+  test("[success] isNovelEpisodeChapter", () => {
+    expect(isNovelEpisodeChapter({ chapterName: "sample chapter title" })).toBe(
+      true,
+    )
+  })
+
+  test("[fail] isNovelEpisodeChapter 1", () => {
+    expect(isNovelEpisodeChapter({})).toBe(false)
+  })
+
+  test("[fail] isNovelEpisodeChapter 2", () => {
+    expect(isNovelEpisodeChapter({ chapterName: null })).toBe(false)
+  })
+
+  test("[fail] isNovelEpisodeChapter 3", () => {
+    expect(isNovelEpisodeChapter({ chapterName: 1234 })).toBe(false)
+  })
+
+  test("[success] isNovelEpisodeTitle", () => {
+    expect(
+      isNovelEpisodeTitle({
+        episodeNo: 1,
+        subtitle: "sample subtitle",
+        upDate: "99991231",
+        isKaitou: false,
+      }),
+    ).toBe(true)
+  })
+
+  test("[fail] isNovelEpisodeTitle 1", () => {
+    expect(
+      isNovelEpisodeTitle({
+        subtitle: "sample subtitle",
+        upDate: "99991231",
+        isKaitou: false,
+      }),
+    ).toBe(false)
   })
 })
