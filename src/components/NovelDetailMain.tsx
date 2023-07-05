5b2a54e2140f9009b63c2a80bad54a3f6c8657ac
sakho13 <wecl.khotaro@gmail.com>
2023-07-05 18:16:40 +0900

    🎨 デザインの修正

--- src/components/NovelDetailMain.tsx
+++ src/components/NovelDetailMain.tsx
@@ -1,6 +1,8 @@
 import { Text, View, ScrollView } from "react-native"
 import { NarouAPINovelPart } from "../types/Narou"
 import PlainTextButton from "./PlainTextButton"
+import PlainText from "./atoms/PlainText"
+import { useSettingsValue } from "../contexts/settingContext"
 
 type Props = {
   novel: NarouAPINovelPart
@@ -15,22 +17,36 @@ const NovelDetailMain: React.FC<Props> = ({
   onTapTry,
   onTapAdd,
 }) => {
+  const { borderColor } = useSettingsValue()
+
   if (!novel)
     return (
       <View>
         <Text>存在しない小説情報です</Text>
+        <PlainTextButton text='閉じる' onTap={onTapClose} icon='times' />
       </View>
     )
 
   return (
-    <View style={{ minHeight: 400 }}>
-      <Text style={{ fontSize: 18 }}>{novel.title}</Text>
+    <View
+      style={{
+        minHeight: 400,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <View style={{ minHeight: 200, maxHeight: 300, marginBottom: 8 }}>
+        <PlainText
+          text={novel.title}
+          styles={{ fontSize: 18, marginHorizontal: 8, marginBottom: 4 }}
+        />
 
-      {/* <ScrollView>
-        <Text>{novel.story}</Text>
-      </ScrollView> */}
+        <ScrollView style={{ paddingTop: 4, borderTopWidth: 1, borderColor }}>
+          <PlainText text={novel.story} />
+        </ScrollView>
+      </View>
 
-      <View style={{ borderTopWidth: 1 }}>
+      <View style={{ borderTopWidth: 1, borderColor }}>
         <View
           style={{
             width: "100%",
@@ -40,12 +56,7 @@ const NovelDetailMain: React.FC<Props> = ({
             justifyContent: "space-around",
           }}
         >
-          <PlainTextButton
-            text='閉じる'
-            onTap={onTapClose}
-            gray={true}
-            icon='times'
-          />
+          <PlainTextButton text='閉じる' onTap={onTapClose} icon='times' />
           <PlainTextButton text='試し読み' onTap={onTapTry} icon='book-open' />
           <PlainTextButton text='追加' onTap={onTapAdd} icon='book' />
         </View>
