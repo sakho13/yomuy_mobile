5b94e397b5075c1fa97a761751c44c91dc791aeb
sakho13 <wecl.khotaro@gmail.com>
2023-07-19 15:08:28 +0900

    🎨 コードデザインの修正

--- src/functions/errorDialog.ts
+++ src/functions/errorDialog.ts
@@ -1,13 +1,5 @@
-import { Alert, AlertButton } from "react-native"
+import { Alert } from "react-native"
 import { LogicError } from "../error/logicError"
-
-export function showError(
-  title: string,
-  message: string,
-  buttons?: AlertButton[],
-) {
-  Alert.alert(title, message, buttons)
-}
 
 export function showLogicError(err: any) {
   if (err instanceof LogicError) {
