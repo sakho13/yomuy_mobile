3a38ff74c34f92fd76da6440223e516a9e311645
sakho13 <wecl.khotaro@gmail.com>
2023-07-19 14:33:41 +0900

    🐛 EpisodeScreenのロード

--- src/screens/EpisodeScreen.tsx
+++ src/screens/EpisodeScreen.tsx
@@ -1,24 +1,40 @@
 import { ActivityIndicator, StyleSheet, View } from "react-native"
 import Honbun from "../components/Honbun"
 import { EpisodeScreenController } from "../controllers/screens/EpisodeScreenController"
-import { useEffect } from "react"
+
+import { useSettingsValue } from "../contexts/settingContext"
+import PlainText from "../components/atoms/PlainText"
 
 const EpisodeScreen: React.FC = () => {
-  const { honbun, loading, fetchHonbun } = EpisodeScreenController()
+  const { honbun, loading, episode, onSwipe } = EpisodeScreenController()
 
-  useEffect(() => {
-    fetchHonbun()
-  }, [])
+  const { backgroundColor, borderColor } = useSettingsValue()
+
+  const backStyle = {
+    backgroundColor,
+    ...StyleSheet.absoluteFillObject,
+  }
 
   if (loading)
     return (
-      <View style={StyleSheet.absoluteFillObject}>
+      <View style={backStyle}>
         <ActivityIndicator />
       </View>
     )
 
   return (
-    <View style={StyleSheet.absoluteFillObject}>
+    <View style={backStyle}>
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderColor,
+          paddingVertical: 8,
+          paddingHorizontal: 4,
+        }}
+      >
+        <PlainText text={`${episode}`} />
+      </View>
+
       <Honbun honbun={honbun} />
     </View>
   )
